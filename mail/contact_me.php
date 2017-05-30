3b11f5d0e8a8e83d5c54a0c24d691bc802dee234
Jaap Jansma <jaap@edeveloper.nl>
2017-05-31 00:19:12 +0200

    first layout

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'henny@jongerenwerkindekerk.nl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$email_body = "Het contact formulier is op je website ingevuld.\n\n"."De volgende gegevens zijn ingevoerd:\n\nNaam: $name\n\nEmail-adres: $email_address\n\nTelefoonnummer: $phone\n\nBericht:\n$message";
+$headers = "From: website@jongerenwerkindekerk.nl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
